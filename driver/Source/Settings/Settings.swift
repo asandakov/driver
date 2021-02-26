27061f64707f88e734a2df29f735a376748e2107
asandakov <66016536+asandakov@users.noreply.github.com>
2021-02-26 15:45:32 +0300

    Update Settings.swift

--- driver/Source/Settings/Settings.swift
+++ driver/Source/Settings/Settings.swift
@@ -13,7 +13,7 @@ struct Settings {
     static var apiURL = "https://alexsandakov.ru/driver/app"
     static var newsURL = "https://tsvet-ryad.ru/articles/"
     static var accessTokenApi = "78172"
-    static var yandexApi = "a6ff3a4c-1ebd-49dc-a8a4-516a8416e586"
+    static var yandexApi = "ваш код"
     static var centerMap = (lat:55.753995, long: 37.614069)
    
     static func getApiUrl() -> URL? {
